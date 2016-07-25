b5016ef9dc6d901463a5b1b89e194d84464d2621
yiming.kuan <yimingwuzere@gmail.com>
2016-07-25 22:35:39 +0800

    Update and rename remove-containers.sh to cleanup.sh
    Added volume deletion and filtering containers/volumes to delete

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+#Deletes all stopped containers and dangling volumes matching the parameters, wildcards supported
+#usage: ./cleanup.sh [container] [volume]
+
+volume_name = "*"
+container_name = "*"
+
+if [ "$2" != "" ]; then
+	volume_name = "$2"
+fi
+
+if [ "$1" != "" ]; then
+	container_name = "$1"
+fi
+
+docker ps -a | grep 'Exited' | grep $container_name| awk '{print $1}' | xargs --no-run-if-empty docker rm
+
+docker volume ls -qf dangling=true | grep $volume_name | docker volume rm $(docker volume ls -qf dangling=true)
--- remove-containers.sh
+++
@@ -1,3 +0,0 @@
-#TODO: add multiple options for container removal. Currently removes all exited containers
-
-docker ps -a | grep 'Exited' | awk '{print $1}' | xargs --no-run-if-empty docker rm
