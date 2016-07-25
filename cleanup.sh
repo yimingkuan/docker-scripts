c3ebaa9a6e41a0e2bc9c5648d85e1660352ba9d1
yiming.kuan <yimingwuzere@gmail.com>
2016-07-25 23:21:55 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,11 @@
 #Deletes all stopped containers and dangling volumes matching the parameters, wildcards supported
 #usage: ./cleanup.sh [container] [volume]
+#TODO: Add alternative for 'xargs -r' for OSX
 
 set -e
 
-volume_name = "\*"
-container_name = "\*"
+volume_name = ""
+container_name = ""
 
 if [ "$2" != "" ]; then
 	$volume_name = "$2"
@@ -15,9 +16,9 @@ if [ "$1" != "" ]; then
 fi
 
 cleanup () {
-	docker ps -a | grep 'Exited' | grep $container_name| awk '{print $1}' | xargs --no-run-if-empty docker rm
+	docker ps -a | grep 'Exited' | grep $container_name | awk '{print $1}' | xargs -r docker rm
 	
-	docker volume ls -qf dangling=true | grep $volume_name | docker volume rm $(docker volume ls -qf dangling=true)
+	docker volume ls -qf dangling=true | grep $volume_name | awk '{print $1}' | xargs -r docker volume rm
 }
 
 cleanup
