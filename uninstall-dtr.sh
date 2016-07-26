60ad63a3783f5e2100c301e1831c03afdbb3e444
yiming.kuan <yimingwuzere@gmail.com>
2016-07-26 22:00:42 +0800

    Update uninstall-dtr.sh

--- uninstall-dtr.sh
+++ uninstall-dtr.sh
@@ -1,4 +1,4 @@
 #Completely uninstalls DTR from the current Docker instance, if present
 
-docker ps -a | grep dtr | grep -v enzi | awk '{print $1}' | xargs docker rm -f
-docker volume ls | grep dtr | awk '{print $2}' | xargs docker volume rm
+docker ps -a | grep dtr | grep -v enzi | awk '{print $1}' | xargs -r docker rm -f
+docker volume ls | grep dtr | awk '{print $2}' | xargs -r docker volume rm
