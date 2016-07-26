3a09c69c3710f302c55b924d95e4a27eec98bc75
yiming.kuan <yimingwuzere@gmail.com>
2016-07-26 21:34:49 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,21 +4,21 @@
 
 set -e
 
-volume_name = ""
-container_name = ""
+volume_name=""
+container_name=""
 
 if [ "$2" != "" ]; then
-	$volume_name = "$2"
+	volume_name="$2"
 fi
 
 if [ "$1" != "" ]; then
-	$container_name = "$1"
+	container_name="$1"
 fi
 
 cleanup () {
-	docker ps -a | grep 'Exited' | grep $container_name | awk '{print $1}' | xargs -r docker rm
+	docker ps -a | grep 'Exited' | grep "$container_name" | awk '{print $1}' | xargs -r docker rm
 	
-	docker volume ls -qf dangling=true | grep $volume_name | awk '{print $1}' | xargs -r docker volume rm
+	docker volume ls -qf dangling=true | grep "$volume_name" | awk '{print $1}' | xargs -r docker volume rm
 }
 
 cleanup
